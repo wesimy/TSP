e7448ab381d9a8e202e56a1d26aab53d83d2706c
Andreas Sigrist <sigiprivat@me.com>
2017-08-09 17:40:30 +0200

    about v1 completed

--- dist/js/app.en.js
+++ dist/js/app.en.js
@@ -21196,6 +21196,8 @@ var map = {
 	"./about-intro.scss": 12,
 	"./animations.scss": 13,
 	"./fonts.scss": 14,
+	"./image-left.scss": 74,
+	"./image-right.scss": 77,
 	"./intro.scss": 15,
 	"./layout.scss": 16,
 	"./managing-director.scss": 17,
@@ -21204,8 +21206,7 @@ var map = {
 	"./page-footer.scss": 20,
 	"./page-header.scss": 21,
 	"./page-media.scss": 22,
-	"./page-sidebar.scss": 23,
-	"./who-we-are.scss": 24
+	"./page-sidebar.scss": 23
 };
 function webpackContext(req) {
 	return __webpack_require__(webpackContextResolve(req));
@@ -21296,12 +21297,7 @@ webpackContext.id = 11;
 // removed by extract-text-webpack-plugin
 
 /***/ }),
-/* 24 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
+/* 24 */,
 /* 25 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -21645,6 +21641,67 @@ var app = (function () {
      app.init();
      
  });
+
+/***/ }),
+/* 27 */,
+/* 28 */,
+/* 29 */,
+/* 30 */,
+/* 31 */,
+/* 32 */,
+/* 33 */,
+/* 34 */,
+/* 35 */,
+/* 36 */,
+/* 37 */,
+/* 38 */,
+/* 39 */,
+/* 40 */,
+/* 41 */,
+/* 42 */,
+/* 43 */,
+/* 44 */,
+/* 45 */,
+/* 46 */,
+/* 47 */,
+/* 48 */,
+/* 49 */,
+/* 50 */,
+/* 51 */,
+/* 52 */,
+/* 53 */,
+/* 54 */,
+/* 55 */,
+/* 56 */,
+/* 57 */,
+/* 58 */,
+/* 59 */,
+/* 60 */,
+/* 61 */,
+/* 62 */,
+/* 63 */,
+/* 64 */,
+/* 65 */,
+/* 66 */,
+/* 67 */,
+/* 68 */,
+/* 69 */,
+/* 70 */,
+/* 71 */,
+/* 72 */,
+/* 73 */,
+/* 74 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 75 */,
+/* 76 */,
+/* 77 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
